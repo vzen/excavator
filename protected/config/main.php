455ea702ace8b11b82534cdd70ed06c299ac2064
Victor Zen <socrates.kisser@gmail.com>
2013-06-24 04:26:26 -0400

    Open Gii up to LAN and WAN requests from LAN

--- protected/config/main.php
+++ protected/config/main.php
@@ -22,15 +22,11 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'password'=>'abc',
+			'ipFilters'=>array('10.0.0.*', gethostbyname('metasw.net')),
 		),
-		*/
 	),
 
 	// application components
