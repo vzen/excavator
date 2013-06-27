f163bcbd30ae1fdad68a3b9f8f1d7c6c28d8a77e
Victor Zen <socrates.kisser@gmail.com>
2013-06-27 08:54:01 -0400

    Implement boilerplate theme

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,7 +4,4 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>YOU WANT DIRT ON THE U.S. GOVERNMENT.</h1>
-
-<p>This app reports public data from federal agencies. If you are a developer, <a href="https://github.com/vzen/excavator">help us add new kinds of dirt</a>.</p>
-
+<h1 class="t-mantle t-centered-text">DIRT ON THE U.S. FEDS.</h1>
